c8745f6dc11506a4fe8ec9c9a06fa7ff2b052e02
SriMoorthy <srimoorthy27@gmail.com>
2024-07-29 15:29:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-function createTable() {
-    //Write your code here
-  
+function createTable(){
+    let rn = prompt('Input number of rows')
+	let cn = prompt('Input number of columns')
+    let table = document.getElementById('myTable')
+    for(let i = 0 ; i < rn ; i++){
+        let newrow = table.insertRow(i);
+        for(let j = 0 ; j < cn ; j++){
+            let newcell = newrow.insertCell(j)
+            newcell.innerHTML = `Row-${i} Column-${j}`
+        }
+    }
+    return table
 }
